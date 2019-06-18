91f3a1a7ed578dc9673bb85151276e6e238800bf
simon3000 <simon3000@163.com>
2019-06-19 05:10:10 +0800

    Create processer.js

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
 GIT_AUTHOR_NAME="nanashi" GIT_AUTHOR_EMAIL="example@example.com" GIT_COMMITTER_NAME="nanashi" GIT_COMMITTER_EMAIL="example@example.com" git add json
+GIT_AUTHOR_NAME="nanashi" GIT_AUTHOR_EMAIL="example@example.com" GIT_COMMITTER_NAME="nanashi" GIT_COMMITTER_EMAIL="example@example.com" git add vtbs
 GIT_AUTHOR_NAME="nanashi" GIT_AUTHOR_EMAIL="example@example.com" GIT_COMMITTER_NAME="nanashi" GIT_COMMITTER_EMAIL="example@example.com" git -c commit.gpgsign=false commit -m "update"
