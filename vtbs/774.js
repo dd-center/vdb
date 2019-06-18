e5cbdbe3077e4a08d13ade6521dab52fa181dfbf
nanashi <example@example.com>
2019-06-19 05:12:46 +0800

    update

--- vtbs/774.js
+++ vtbs/774.js
@@ -3,6 +3,7 @@ module.exports = {
     jp: '774',
     cn: '774',
   },
+  type: 'unknow',
   '2d': true,
   accounts: {
     twitter: '______774______',
--- vtbs/パンディ.js
+++ vtbs/パンディ.js
@@ -6,7 +6,6 @@ module.exports = {
   '2d': true,
   accounts: {
     youtube: 'UCu5DbFfYe3KiwhcmanCvSVg',
-    bilibili: '9986117',
     twitter: 'pandei_ch',
     userlocal: '62E80546C414CBC6_91a480',
   },
