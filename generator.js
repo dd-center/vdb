78d5e3af50485da1796ac19e8e5e8b420fc6f4bb
simon3000 <simon3000@163.com>
2019-06-19 21:05:21 +0800

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,5 +1,26 @@
 const { writeFile } = require('fs').promises
 
 require('.')
-  .then(JSON.stringify)
-  .then(e => writeFile('json/list.json', e))
+  .then(async e => {
+    await writeFile('json/list.json', JSON.stringify(e))
+    let knowMid = {}
+    let bilibili = []
+    e.vtbs.forEach(({ accounts, name }) => {
+      accounts
+        .filter(({ platform }) => platform === 'bilibili')
+        .forEach(({ id }) => {
+          if (!knowMid[id]) {
+            knowMid[id] = true
+            bilibili.push({
+              mid: Number(id),
+              note: Object.entries(name)
+                .filter(([lang, name]) => lang !== 'extra')
+                .filter(([lang, name]) => lang !== 'default')
+                .map(([lang, name]) => name)
+                .concat(name.extra),
+            })
+          }
+        })
+    })
+    await writeFile('json/vtbs.moe.json', JSON.stringify(bilibili))
+  })
