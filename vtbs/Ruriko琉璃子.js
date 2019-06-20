fbc1378aadaaf22c8091597a6deb777d52979fb2
simon3000 <simon3000@163.com>
2019-06-20 09:30:48 +0800

    update
    close https://github.com/bilibili-dd-center/vtbs.moe/issues/39

---
+++ vtbs/Ruriko琉璃子.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: {
+    cn: 'Ruriko琉璃子',
+  },
+  '2d': true,
+  accounts: {
+    bilibili: '47441',
+  },
+}
---
+++ vtbs/能美枫铃だよ.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: {
+    jp: '能美枫铃だよ',
+  },
+  '2d': true,
+  accounts: {
+    bilibili: '2945435',
+  },
+}
