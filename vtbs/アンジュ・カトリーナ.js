0e931655872886c29f92d039dbfebebf0e5f8ef5
simon3000 <simon3000@163.com>
2019-06-20 09:17:06 +0800

    update
    close https://github.com/bilibili-dd-center/vtbs.moe/issues/37

--- vtbs/アンジュ・カトリーナ.js
+++ vtbs/アンジュ・カトリーナ.js
@@ -1,11 +1,14 @@
 module.exports = {
   name: {
     jp: 'アンジュ・カトリーナ',
+    en: 'Ange Katrina',
+    cn: '安洁·卡特莉娜',
   },
   '2d': true,
   accounts: {
     youtube: 'UCHVXbQzkl3rDfsXWo8xi2qw',
     twitter: 'Ange_Katrina_',
     userlocal: '0C8CB4DC6306C469_ba69b8',
+    bilibili: { id: '396429244', type: 'relay' },
   },
 }
--- vtbs/リゼ・ヘルエスタ.js
+++ vtbs/リゼ・ヘルエスタ.js
@@ -7,5 +7,6 @@ module.exports = {
     youtube: 'UCZ1xuCK1kNmn5RzPYIZop3w',
     twitter: 'Lize_Helesta',
     userlocal: '1E2B7FEC0064F8B7_3f316b',
+    bilibili: { id: '412180051', type: 'relay' },
   },
 }
