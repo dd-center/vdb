5d03cbfbac8d33c5d36e59ec457d0be86eb63ca8
simon3000 <simon3000@163.com>
2019-07-03 17:13:41 +0200

    add
    close #1

--- vtbs/葛葉.js
+++ vtbs/葛葉.js
@@ -1,11 +1,13 @@
 module.exports = {
   name: {
     jp: '葛葉',
+    cn: '葛叶',
   },
   '2d': true,
   accounts: {
     youtube: 'UCSFCh5NL4qXrAy9u-u2lX3g',
     twitter: 'Vamp_Kuzu',
     userlocal: 'DC974C92F15ECEA8_929a0b',
+    bilibili: '434563422',
   },
 }
