924ada5fe04256d1da796d1df696c28872b3a014
nanashi <example@example.com>
2019-07-06 18:56:32 +0200

    update

---
+++ vtbs/星空モノカ.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: {
+    jp: '星空モノカ',
+  },
+  '2d': true,
+  accounts: {
+    youtube: 'UC4nG8KLbdyCQh3Qmineic6w',
+    twitter: 'monokavtuber',
+    bilibili: { id: '440464475', type: 'relay' },
+  },
+}
