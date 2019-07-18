ca655eca1f6db45ba1c8ad38e98a2c671cd0047a
simon3000 <simon3000@163.com>
2019-07-18 15:03:12 +0200

    hololive 2nd

---
+++ vtbs/兎田ぺこら.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: {
+    jp: '兎田ぺこら',
+    en: 'Usada Pekora',
+  },
+  '2d': true,
+  accounts: {
+    youtube: 'UC1DCedRgGHBdm81E1llLhOQ',
+    twitter: 'usadapekora',
+  },
+}
---
+++ vtbs/潤羽るしあ.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: {
+    jp: '潤羽るしあ',
+    cn: '润羽露西娅',
+  },
+  '2d': true,
+  accounts: {
+    youtube: 'UCl_gCybOJRIgOXw6Qb4qJzQ',
+    twitter: 'uruharushia',
+    bilibili: '443300418',
+  },
+}
