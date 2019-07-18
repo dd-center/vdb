94f4895e4603f5143bb502815100c6e776a77b0f
simon3000 <simon3000@163.com>
2019-07-18 14:02:00 +0200

    add list.uncompressed.json

--- generator.js
+++ generator.js
@@ -3,6 +3,7 @@ const { writeFile } = require('fs').promises
 require('.')
   .then(async e => {
     await writeFile('json/list.json', JSON.stringify(e))
+    await writeFile('json/list.uncompressed.json', JSON.stringify(e, undefined, 2))
     let knowMid = {}
     let bilibili = []
     e.vtbs.forEach(({ accounts, name, uuid }) => {
