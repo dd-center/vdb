c63b33b11fca3296e6b237452d243277833b2fb3
simon3000 <simon3000@163.com>
2019-07-24 16:31:13 +0200

    Create 音音継ねね.js
    close #6

---
+++ vtbs/音音継ねね.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: {
+    jp: '音々継ねね',
+    cn: '音音继nene',
+  },
+  '2d': true,
+  accounts: {
+    youtube: 'UCd6_vOp6VmWVuUipxdj2tbQ',
+    bilibili: '439071630',
+    showroom: 'nenenenetsugu',
+  },
+}
