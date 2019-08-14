f3f364dcf6db2acd3691475c05e8b30513c37944
brainbush <960821@gmail.com>
2019-08-14 20:41:09 +0800

    VirtuaReal 三期

---
+++ vtbs/VirtuaReal-正在准备中.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: {
+    cn: 'VirtuaReal-正在准备中',
+  },
+  '2d': true,
+  accounts: {
+    bilibili: '455965041',
+  },
+}
---
+++ vtbs/无理MURI.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: {
+    cn: '无理MURI',
+  },
+  '2d': true,
+  accounts: {
+    bilibili: '455916618',
+  },
+}
