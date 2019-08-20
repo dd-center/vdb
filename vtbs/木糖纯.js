7b12bc627b145fa32f37a2bf5afcaec121be5d4c
simon3000 <simon3000@163.com>
2019-08-20 18:43:21 +0200

    木糖纯和庄不纯 -> 木糖纯

---
+++ vtbs/木糖纯.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: {
+    cn: '木糖纯',
+  },
+  '2d': true,
+  accounts: {
+    bilibili: ['54191665'],
+  },
+}
--- vtbs/木糖纯和庄不纯.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: {
-    cn: '木糖纯和庄不纯',
-  },
-  '2d': true,
-  accounts: {
-    bilibili: ['54191665', '20530305'],
-  },
-}
