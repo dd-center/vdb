74671580a9d3297768cc454e64b3c166955015de
simon3000 <simon3000@163.com>
2019-08-29 14:40:46 +0200

    Update アンジュ・カトリーナ.js

--- vtbs/アンジュ・カトリーナ.js
+++ vtbs/アンジュ・カトリーナ.js
@@ -9,6 +9,6 @@ module.exports = {
     youtube: 'UCHVXbQzkl3rDfsXWo8xi2qw',
     twitter: 'Ange_Katrina_',
     userlocal: '0C8CB4DC6306C469_ba69b8',
-    bilibili: { id: '396429244', type: 'relay' },
+    bilibili: '458154143',
   },
 }
