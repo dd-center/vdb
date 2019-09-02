e11b258a7dee3316d419aec0fb16240d73d39105
simon3000 <simon3000@163.com>
2019-09-02 07:18:16 +0200

    update
    close https://github.com/bilibili-dd-center/vtbs.moe/issues/90

---
+++ vtbs/星野由乃.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: {
+    cn: '星野由乃',
+  },
+  '2d': true,
+  accounts: {
+    bilibili: '8576944',
+  },
+}
---
+++ vtbs/某柯.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: {
+    cn: '某柯',
+    en: 'moke',
+  },
+  '2d': true,
+  accounts: {
+    bilibili: '2844993',
+  },
+}
