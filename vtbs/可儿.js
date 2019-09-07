e03f31e8cb4a3c2c0f657b78509546e258d1669f
simon3000 <simon3000@163.com>
2019-09-07 05:22:29 +0200

    Create 可儿.js
    close #49

--- vtbs/可儿.js
+++ vtbs/可儿.js
@@ -1,10 +1,11 @@
 module.exports = {
   name: {
-    cn: '凉宫白樱',
+    cn: '可儿',
+    extra: '凉宫白樱',
   },
   '2d': true,
   accounts: {
-    bilibili: '244407176',
+    bilibili: ['481169', '244407176'],
     weibo: '凉宫白樱_虚拟动漫社计划',
   },
 }
