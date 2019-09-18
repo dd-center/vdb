0830581a3a4ba2fa5f27e311ccf1f4aeede53dc2
simon3000 <simon3000@163.com>
2019-09-18 17:12:10 +0200

    update
    close #65

---
+++ vtbs/轻又.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: {
+    cn: '轻又',
+  },
+  '2d': true,
+  accounts: {
+    pixiv: '3449894',
+    twitter: 'neovialotus',
+    afdian: 'neovia',
+    bilibili: '826101',
+    youtube: 'UCELb-uMjz0Bbtlg5qHooGDA',
+  },
+}
