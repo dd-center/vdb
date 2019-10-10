91926295723943bf226bd5c285c970d2e2f87eaf
nanashi <example@example.com>
2019-10-10 10:53:25 +0200

    update

---
+++ vtbs/Anastasia.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: {
+    en: 'Anastasia',
+  },
+  '2d': true,
+  accounts: {
+    bilibili: '14269054',
+  },
+}
---
+++ vtbs/云祈.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: {
+    cn: '云祈',
+    en: 'kinori',
+  },
+  '2d': true,
+  accounts: {
+    bilibili: '3406546',
+  },
+}
