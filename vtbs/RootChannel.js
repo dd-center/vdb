cdeb315b884c32a7abdd8fd0a72ccca9761ba1a6
nanashi <example@example.com>
2019-10-13 10:03:10 +0200

    update

--- vtbs/RootChannel.js
+++ vtbs/RootChannel.js
@@ -3,6 +3,7 @@ module.exports = {
     cn: '树根',
   },
   '2d': true,
+  group: 'prprclub',
   accounts: {
     bilibili: '2304086',
     twitter: 'ChannelRoot'
---
+++ vtbs/小狼.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: {
+    cn: '小狼',
+  },
+  '2d': true,
+  accounts: {
+    weibo: 'myglz',
+    bilibili: '298484',
+  },
+}
