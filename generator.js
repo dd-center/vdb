4ba4bd54c5c3633bd36a24a48be6c5b0e199ff4b
simon3000 <simon30002021@icloud.com>
2019-10-18 22:38:06 +0200

    Delete vtbs.moe.json

--- generator.js
+++ generator.js
@@ -4,25 +4,4 @@ require('.')
   .then(async e => {
     await writeFile('json/list.json', JSON.stringify(e))
     await writeFile('json/list.uncompressed.json', JSON.stringify(e, undefined, 2))
-    let knowMid = {}
-    let bilibili = []
-    e.vtbs.forEach(({ accounts, name, uuid }) => {
-      accounts
-        .filter(({ platform }) => platform === 'bilibili')
-        .forEach(({ id }) => {
-          if (!knowMid[id]) {
-            knowMid[id] = true
-            bilibili.push({
-              mid: Number(id),
-              uuid,
-              note: Object.entries(name)
-                .filter(([lang, name]) => lang !== 'extra')
-                .filter(([lang, name]) => lang !== 'default')
-                .map(([lang, name]) => name)
-                .concat(name.extra),
-            })
-          }
-        })
-    })
-    await writeFile('json/vtbs.moe.json', JSON.stringify(bilibili))
   })
