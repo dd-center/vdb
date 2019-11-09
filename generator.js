ee8fc0d0e218ef5552d08b1762b6533562b4e46d
simon3000 <simon30002021@icloud.com>
2019-11-09 06:12:50 +0100

    normalize

--- generator.js
+++ generator.js
@@ -1,6 +1,26 @@
-const { writeFile } = require('fs').promises
+const { readFile, writeFile, readdir, unlink } = require('fs').promises
+const { join } = require('path')
 
-require('.')
+readdir('vtbs')
+  .then(async dirs => {
+    await dirs
+      .map(dir => join('vtbs', dir))
+      .map(path => ({ path, contentP: readFile(path) }))
+      .map(async ({ path, contentP }) => {
+        const content = await contentP
+        if (path.normalize() !== path) {
+          console.log(path)
+        }
+        await unlink(path)
+        await writeFile(path.normalize(), content)
+      })
+      .reduce(async (a, b) => {
+        await a
+        return b
+      })
+
+    return require('.')
+  })
   .then(async e => {
     await writeFile('json/list.json', JSON.stringify(e))
     await writeFile('json/list.uncompressed.json', JSON.stringify(e, undefined, 2))
