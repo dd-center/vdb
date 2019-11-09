114beae3c2032b59a3d234b56a8c445878a9bcd9
simon3000 <simon30002021@icloud.com>
2019-11-09 06:13:54 +0100

    Update generator.js

--- generator.js
+++ generator.js
@@ -2,25 +2,22 @@ const { readFile, writeFile, readdir, unlink } = require('fs').promises
 const { join } = require('path')
 
 readdir('vtbs')
-  .then(async dirs => {
-    await dirs
-      .map(dir => join('vtbs', dir))
-      .map(path => ({ path, contentP: readFile(path) }))
-      .map(async ({ path, contentP }) => {
-        const content = await contentP
-        if (path.normalize() !== path) {
-          console.log(path)
-        }
-        await unlink(path)
-        await writeFile(path.normalize(), content)
-      })
-      .reduce(async (a, b) => {
-        await a
-        return b
-      })
-
-    return require('.')
-  })
+  .then(async dirs => dirs
+    .map(dir => join('vtbs', dir))
+    .map(path => ({ path, contentP: readFile(path) }))
+    .map(async ({ path, contentP }) => {
+      const content = await contentP
+      if (path.normalize() !== path) {
+        console.log(path)
+      }
+      await unlink(path)
+      await writeFile(path.normalize(), content)
+    })
+    .reduce(async (a, b) => {
+      await a
+      return b
+    }))
+  .then(() => require('.'))
   .then(async e => {
     await writeFile('json/list.json', JSON.stringify(e))
     await writeFile('json/list.uncompressed.json', JSON.stringify(e, undefined, 2))
