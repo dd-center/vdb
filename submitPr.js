953c8d1281222132b0d0c069b81d6e9462a02c80
simon3000 <simon30002021@icloud.com>
2020-04-07 14:35:42 +0200

    test submit pr

--- submitPr.js
+++ submitPr.js
@@ -1,3 +1,20 @@
-const { ISSUE_NUMBER, ISSUE_BODY } = process.env
+const { GitProcess } = require('dugite')
 
-console.log({ ISSUE_NUMBER, ISSUE_BODY })
+const { ISSUE_NUMBER, ISSUE_BODY, GITHUB_TOKEN, GITHUB_ACTOR } = process.env
+
+const branchName = `submit-${ISSUE_NUMBER}`
+
+const remote = `https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/dd-center/vdb.git`
+
+const gitExec = async (...params) => {
+  const { stdout } = await GitProcess.exec(params, process.cwd())
+  console.log(stdout)
+}
+
+;
+
+(async () => {
+  await gitExec('branch', branchName)
+  await gitExec('checkout', branchName)
+  await gitExec('push', '--set-upstream', remote, branchName)
+})()
