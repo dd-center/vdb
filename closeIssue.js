05f2d28e147d1d750241523ae104c81896a13ebd
simon3000 <simon30002021@icloud.com>
2020-04-09 18:58:24 +0200

    log close Issue

--- closeIssue.js
+++ closeIssue.js
@@ -2,11 +2,16 @@ const got = require('got')
 
 const { NUMBER, GITHUB_TOKEN } = process.env
 
-got(`https://api.github.com/repos/dd-center/vdb/pulls/${NUMBER}`).json()
+const log = w => {
+  console.log(w)
+  return w
+}
+
+got(log(`https://api.github.com/repos/dd-center/vdb/pulls/${NUMBER}`)).json()
   .then(async ({ head: { label }, user: { login } }) => {
     if (login === 'github-actions[bot]' && label.startsWith('dd-center:submit-')) {
       const issueNumber = Number(label.replace('dd-center:submit-', ''))
-      await got.post(`https://api.github.com/repos/dd-center/vdb/issues/${issueNumber}/comments`, {
+      await got.post(log(`https://api.github.com/repos/dd-center/vdb/issues/${issueNumber}/comments`), {
         json: {
           body: `closing since #${NUMBER} is closed`,
         },
@@ -14,7 +19,7 @@ got(`https://api.github.com/repos/dd-center/vdb/pulls/${NUMBER}`).json()
           authorization: `Bearer ${GITHUB_TOKEN}`,
         },
       })
-      await got.patch(`https://api.github.com/repos/dd-center/vdb/issues/${issueNumber}`, {
+      await got.patch(log(`https://api.github.com/repos/dd-center/vdb/issues/${issueNumber}`), {
         json: {
           state: 'closed',
         },
