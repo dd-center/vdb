ac75e3398dbcef28edb498be4139c7ac68760d3e
nanashi <example@example.com>
2020-04-26 13:30:08 +0000

    deploy: 5190e694fd5a2d07f7b8cf3932f7032003e96ad2

--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
+const { readdir } = require('fs').promises
+
 const test = require('.')
 
-require('..').then(vdb => {
-  const errors = test(vdb)
+require('..').then(async vdb => {
+  const errors = test(vdb, await readdir('vtbs'))
   if (errors.length) {
     errors.forEach(e => console.error(e))
     process.exit(1)
