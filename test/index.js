976b3968034adc07787124c05e2686d0481ff817
simon3000 <simon30002021@icloud.com>
2020-09-23 00:57:21 +0200

    filename space check

--- test/index.js
+++ test/index.js
@@ -4,7 +4,8 @@ const platform = require('./platform')
 const id = require('./id')
 const account = require('./account')
 const filename = require('./filename')
+const filenameSpace = require('./filename-space')
 
-const tests = [group, repeat, id, platform, account, filename]
+const tests = [group, repeat, id, platform, account, filename, filenameSpace]
 
 module.exports = (vdb, files) => tests.flatMap(f => f(vdb, files))
