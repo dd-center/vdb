fb2f1c6cf37445085799fbb6b84e133d78a0f0c7
simon3000 <simon30002021@icloud.com>
2020-09-23 02:19:07 +0200

    name test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const { readdir } = require('fs').promises
 
 const test = require('.')
 
-require('..').then(async vdb => {
+require('..').withFilename().then(async vdb => {
   const errors = test(vdb, await readdir('vtbs'))
   if (errors.length) {
     errors.forEach(e => console.error(e))
