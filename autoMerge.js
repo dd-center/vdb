c61b56d0f6e5de06b5bf262be8d3644d84e83991
simon3000 <simon30002021@icloud.com>
2021-04-08 06:56:53 +0800

    Update autoMerge.js

--- autoMerge.js
+++ autoMerge.js
@@ -6,13 +6,16 @@ const client = github.getOctokit(GITHUB_TOKEN)
 
 // graphql code from from https://github.com/alexwilson/enable-github-automerge-action/blob/bd3f7322b319420f2ade75d057a74a535baf7eaf/src/enable-github-automerge-action.ts#L113-L141
 
-client.graphql(`
-  mutation(
-    $NUMBER: ID!
-  ) {
-      enablePullRequestAutoMerge(input: {
-        pullRequestId: $NUMBER
-      }) {
-        clientMutationId
-      }
-  }`, { NUMBER })
+client.graphql(`{
+  repository(name: "vdb", owner: "dd-center") {
+    pullRequest(number: ${NUMBER}) {
+      id
+    }
+  }
+}
+`).then(({ repository: { pullRequest: { id } } }) => client.graphql(`
+  mutation( $id: ID! ) {
+    enablePullRequestAutoMerge(input: { pullRequestId: $id }) {
+      clientMutationId
+    }
+  }`, { id }))
