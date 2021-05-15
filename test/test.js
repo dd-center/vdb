1ebf383e54151baff6bafb2c6365efb96228955f
nanashi <example@example.com>
2021-05-15 13:52:52 +0000

    deploy: 335f80cd36caaeee40c82ddbd6418080356853ea

--- test/test.js
+++ test/test.js
@@ -1,11 +1,14 @@
 const { readdir } = require('fs').promises
 
 const test = require('.')
-
-require('..').withFilename().then(async vdb => {
-  const errors = test(vdb, await readdir('vtbs'))
-  if (errors.length) {
-    errors.forEach(e => console.error(e))
+require('..').withFilename()
+  .then(async vdb => {
+    const errors = test(vdb, await readdir('vtbs'))
+    if (errors.length) {
+      errors.forEach(e => console.error(e))
+      process.exit(1)
+    }
+  }).catch(e => {
+    console.error(e)
     process.exit(1)
-  }
-})
+  })
