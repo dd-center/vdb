5ce1e30ffa27ba877c981004e13dc24001bd3c61
simon3000 <simon30002021@icloud.com>
2021-05-15 21:50:14 +0800

    ??

--- test/action-test.js
+++ test/action-test.js
@@ -3,11 +3,15 @@ const core = require('@actions/core')
 
 const test = require('.')
 
-require('..').then(async vdb => {
-  const errors = test(vdb, await readdir('vtbs'))
-  if (errors.length) {
-    const error = errors.join('\n')
-    core.setFailed(`测试错误:
+require('..')
+  .then(async vdb => {
+    const errors = test(vdb, await readdir('vtbs'))
+    if (errors.length) {
+      const error = errors.join('\n')
+      core.setFailed(`测试错误:
 ${error}`)
-  }
-})
+    }
+  }).catch(e => {
+    core.setFailed(`测试错误:
+${e}`)
+  })
