477b62ff97dd511097539899a8e2cc28a97e9a34
nanashi <example@example.com>
2022-12-22 16:50:35 +0000

    deploy: 8262b26bff6d229f3de303f627f7887b6be16972

--- closeIssue.js
+++ closeIssue.js
@@ -11,14 +11,6 @@ got(log(`https://api.github.com/repos/dd-center/vdb/pulls/${NUMBER}`)).json()
   .then(async ({ head: { label }, user: { login } }) => {
     if (login === 'github-actions[bot]' && label.startsWith('dd-center:submit-')) {
       const issueNumber = Number(label.replace('dd-center:submit-', ''))
-      await got.post(log(`https://api.github.com/repos/dd-center/vdb/issues/${issueNumber}/comments`), {
-        json: {
-          body: `closing since #${NUMBER} is closed`,
-        },
-        headers: {
-          authorization: `Bearer ${GITHUB_TOKEN}`,
-        },
-      })
       await got.patch(log(`https://api.github.com/repos/dd-center/vdb/issues/${issueNumber}`), {
         json: {
           state: 'closed',
