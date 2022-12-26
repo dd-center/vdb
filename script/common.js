f5705b3c6fa40db2876b6d8f8333d51310a5403a
nanashi <example@example.com>
2022-12-26 17:22:56 +0000

    deploy: 4753782c2c7844dcc1cabbe2e05d7249e1ec3289

--- script/common.js
+++ script/common.js
@@ -1,3 +1,17 @@
+const { GitProcess } = require('dugite')
+
+const gitExec = async (params, { name = 'vdb-bot', email = 'tend.runnier0q@icloud.com' } = {}) => {
+  const { stdout, stderr } = await GitProcess.exec(params, process.cwd(), {
+    env: {
+      GIT_AUTHOR_NAME: name,
+      GIT_AUTHOR_EMAIL: email,
+      GIT_COMMITTER_NAME: name,
+      GIT_COMMITTER_EMAIL: email,
+    },
+  })
+  console.log({ stdout, stderr })
+}
+
 const decodeBase64 = base64 => String(Buffer.from(base64, 'base64'))
 
 const decodeBlock = block => block
@@ -6,6 +20,7 @@ const decodeBlock = block => block
   .map(([command, arg, extra = '']) => [command, decodeBase64(arg), decodeBase64(extra)])
 
 module.exports = {
+  gitExec,
   decodeBase64,
   decodeBlock
 }
