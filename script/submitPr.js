de0e11915ebb61b48c0177af64bc6334c9da010d
nanashi <example@example.com>
2023-06-29 07:25:57 +0000

    deploy: 56d648a6a0367abeae6eaea5b27ff6d0e9926db7

--- script/submitPr.js
+++ script/submitPr.js
@@ -1,11 +1,12 @@
 const { writeFile, unlink } = require('fs').promises
 const { join } = require('path')
+const { EOL } = require('os')
 
 const { GitProcess } = require('dugite')
 
 const { decodeBase64, decodeBlock } = require('./common')
 
-const { ISSUE_NUMBER, ISSUE_BODY } = process.env
+const { ISSUE_NUMBER, ISSUE_BODY, GITHUB_OUTPUT } = process.env
 
 const saveName = join('vtbs-review', `${ISSUE_NUMBER}.json`)
 
@@ -32,6 +33,8 @@ const gitExec = async (params, { name = 'nanashi', email = 'example@example.com'
     await gitExec(['add', 'vtbs-review'], gitUser)
     await gitExec(['add', 'vtbs'], gitUser)
     await gitExec(['commit', '-m', title, '-m', ISSUE_BODY, '-m', `close #${ISSUE_NUMBER}`], gitUser)
+    await writeFile(GITHUB_OUTPUT, `title=${title}${EOL}`)
+    await writeFile(GITHUB_OUTPUT, `body=${ISSUE_BODY}\nclose #${ISSUE_NUMBER}${EOL}`)
   }
   if (block) {
     await unlink(saveName).catch(() => {})
